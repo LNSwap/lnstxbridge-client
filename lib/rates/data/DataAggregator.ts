a9a65297f9175a9471d1948737476b1880e7952c
pseudozach <zozsoy@gmail.com>
2022-05-16 19:37:46 +0000

    add AlexResillientOracle as data source

--- lib/rates/data/DataAggregator.ts
+++ lib/rates/data/DataAggregator.ts
@@ -9,6 +9,7 @@ import Coingecko from './exchanges/Coingecko';
 import ArkadikoOracle from './exchanges/ArkadikoOracle';
 import Okcoin from './exchanges/Okcoin';
 import AlexOracle from './exchanges/AlexOracle';
+import AlexResillientOracle from './exchanges/AlexResillientOracle';
 
 class DataAggregator {
   private readonly exchanges: Exchange[] = [
@@ -21,6 +22,7 @@ class DataAggregator {
     new ArkadikoOracle(),
     new Okcoin(),
     new AlexOracle(),
+    new AlexResillientOracle(),
   ];
 
   public readonly pairs = new Set<[string, string]>();
